2194e65ce15483b05aa7d7553c64b423ba5bd988
Tommaso <tommaso.lapenna@edu.unifi.it>
2026-07-09 19:12:56 +0200

    Fixed account_user table admin file

--- accounts/models.py
+++ accounts/models.py
@@ -10,5 +10,5 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
 
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
