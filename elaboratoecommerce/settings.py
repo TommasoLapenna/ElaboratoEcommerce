ca9f2f518f5c5e21f2aba0fef44392f21ff76367
Tommaso <tommaso.lapenna@edu.unifi.it>
2026-07-10 16:46:14 +0200

    commit for render deployment

--- elaboratoecommerce/settings.py
+++ elaboratoecommerce/settings.py
@@ -95,15 +95,15 @@ WSGI_APPLICATION = 'elaboratoecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #'default': dj_database_url.config(
-    #    # Replace this value with your local database's connection string.
-    #    default='postgresql://postgres:postgres@localhost:5432/mysite',
-    #    conn_max_age=600
-    #)
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
